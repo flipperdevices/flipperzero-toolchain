a87df0ed35ae0ab4b4fd1070363f7a0d196b80f1
DrunkBatya <drunkbatya.js@gmail.com>
2022-06-16 18:02:56 -0400

    windows gdb-py wrapper fix

--- src/win32/wrappers/arm-none-eabi-gdb-py-wrapper.bat
+++ src/win32/wrappers/arm-none-eabi-gdb-py-wrapper.bat
@@ -1,3 +1,3 @@
 @echo off
 set "toolchainRoot=%~dp0.."
-cmd /V /C "set "HOME=%USERPROFILE%" && set "PATH=%PATH%;%toolchainRoot%\python" && set "PYTHONHOME=%PATH%;%toolchainRoot%\python" && %~dp0arm-none-eabi-gdb-py-bin.exe %*"
+cmd /V /C "set "HOME=%USERPROFILE%" && set "PATH=%toolchainRoot%\python;%PATH%" && set "PYTHONHOME=%toolchainRoot%\python" && %~dp0arm-none-eabi-gdb-py-bin.exe %*"
