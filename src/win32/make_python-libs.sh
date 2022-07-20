6e7c557435fcdc9ba9dd0f15839f7b3d8c0e36aa
DrunkBatya <drunkbatya.js@gmail.com>
2022-07-20 12:46:20 -0400

    add Black directly to toolchain

--- src/win32/make_python-libs.sh
+++ src/win32/make_python-libs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 mkdir arch
+git init
+git add .
+git commit -m "init"
+./python.exe -m pip install protobuf==3.20.1 pyserial==3.5 heatshrink2==0.11.0 Pillow==9.1.1 python3-protobuf==2.5.0 black==22.6.0
 cp --parents -r $(git status | grep -v " " | awk '{print $1}') arch/
 cd arch
 tar -cvf ../python-libs.tar.gz *
