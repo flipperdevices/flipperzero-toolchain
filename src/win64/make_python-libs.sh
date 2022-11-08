6c474c4932397548b3a78292e06548a8844dbcce
DrunkBatya <drunkbatya.js@gmail.com>
2022-11-08 09:22:20 -0500

    Add packaging to python, bump version to 19, upgrade protobuf

--- src/win64/make_python-libs.sh
+++ src/win64/make_python-libs.sh
@@ -5,7 +5,7 @@ git init
 git add .
 git commit -m "init"
 ./python.exe -m pip install --upgrade pip
-./python.exe -m pip install protobuf==3.20.1 pyserial==3.5 heatshrink2==0.11.0 Pillow==9.1.1 python3-protobuf==2.5.0 black==22.6.0 ansi==0.3.6 SCons==4.4.0 colorlog==6.7.0
+./python.exe -m pip install protobuf==4.21.7 pyserial==3.5 heatshrink2==0.11.0 Pillow==9.1.1 python3-protobuf==2.5.0 black==22.6.0 ansi==0.3.6 SCons==4.4.0 colorlog==6.7.0 packaging==21.3
 cp --parents -r $(git status | grep -v " " | awk '{print $1}') arch/
 cd arch
 tar -cvf ../python-libs.tar.gz *
