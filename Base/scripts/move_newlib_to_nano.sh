9491e1dcf57dfbbedbb48cfa08a8f57317e0e574
DrunkBatya <drunkbatya.js@gmail.com>
2023-11-14 17:04:37 +0000

    Fix

--- Base/scripts/move_newlib_to_nano.sh
+++ Base/scripts/move_newlib_to_nano.sh
@@ -7,19 +7,43 @@ NEWLIB_NANO_TEMP_ROOT=/toolchain/newlib-nano-temp
 
 CPUS="$(grep -c processor /proc/cpuinfo )";
 
-function move_newlib_to_nano() {
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/libstdc++.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/libstdc++_nano.a";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/libsupc++.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/libsupc++_nano.a";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/libc.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/libc_nano.a";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/libg.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/libg_nano.a";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/librdimon.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/librdimon_nano.a";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/nano.specs" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/nano.specs";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/rdimon.specs" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/rdimon.specs";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/nosys.specs" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/nosys.specs";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib"/*crt0.o "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/";
+# Copy target libraries from each multilib directories.
+# Usage copy_multi_libs dst_prefix=... src_prefix=... target_gcc=...
+copy_multi_libs() {
+    local -a multilibs
+    local multilib
+    local multi_dir
+    local src_prefix
+    local dst_prefix
+    local src_dir
+    local dst_dir
+    local target_gcc
 
-    mkdir -p "$LINUX_OUTPUT_ROOT/arm-none-eabi/include/newlib-nano";
-    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/include/newlib.h" "$LINUX_OUTPUT_ROOT/arm-none-eabi/include/newlib-nano/newlib.h";
+    for arg in "$@" ; do
+        eval "${arg// /\\ }"
+    done
+
+    multilibs=( $("${target_gcc}" -print-multi-lib 2>/dev/null) )
+    for multilib in "${multilibs[@]}" ; do
+        multi_dir="${multilib%%;*}"
+        src_dir=${src_prefix}/${multi_dir}
+        dst_dir=${dst_prefix}/${multi_dir}
+        cp -f "${src_dir}/libstdc++.a" "${dst_dir}/libstdc++_nano.a"
+        cp -f "${src_dir}/libsupc++.a" "${dst_dir}/libsupc++_nano.a"
+        cp -f "${src_dir}/libc.a" "${dst_dir}/libc_nano.a"
+        cp -f "${src_dir}/libg.a" "${dst_dir}/libg_nano.a"
+        cp -f "${src_dir}/librdimon.a" "${dst_dir}/librdimon_nano.a"
+        cp -f "${src_dir}/nano.specs" "${dst_dir}/"
+        cp -f "${src_dir}/rdimon.specs" "${dst_dir}/"
+        cp -f "${src_dir}/nosys.specs" "${dst_dir}/"
+        cp -f "${src_dir}/"*crt0.o "${dst_dir}/"
+    done
 }
 
-move_newlib_to_nano;
+copy_multi_libs src_prefix="$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib" \
+		dst_prefix="$LINUX_OUTPUT_ROOT/arm-none-eabi/lib" \
+		target_gcc="$LINUX_OUTPUT_ROOT/bin/arm-none-eabi-gcc";
+
+mkdir -p "$LINUX_OUTPUT_ROOT/arm-none-eabi/include/newlib-nano";
+mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/include/newlib.h" \
+	"$LINUX_OUTPUT_ROOT/arm-none-eabi/include/newlib-nano/newlib.h";
