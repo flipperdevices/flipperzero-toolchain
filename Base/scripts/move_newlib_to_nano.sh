3394b177fb0924a68777a80c1497f451872f1e66
DrunkBatya <drunkbatya.js@gmail.com>
2023-11-14 13:30:11 +0000

    Add working build again

--- Base/scripts/move_newlib_to_nano.sh
+++ Base/scripts/move_newlib_to_nano.sh
@@ -13,6 +13,10 @@ function move_newlib_to_nano() {
     mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/libc.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/libc_nano.a";
     mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/libg.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/libg_nano.a";
     mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/librdimon.a" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/librdimon_nano.a";
+    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/nano.specs" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/nano.specs";
+    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/rdimon.specs" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/rdimon.specs";
+    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib/nosys.specs" "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/nosys.specs";
+    mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/lib"/*crt0.o "$LINUX_OUTPUT_ROOT/arm-none-eabi/lib/";
 
     mkdir -p "$LINUX_OUTPUT_ROOT/arm-none-eabi/include/newlib-nano";
     mv "$NEWLIB_NANO_TEMP_ROOT/arm-none-eabi/include/newlib.h" "$LINUX_OUTPUT_ROOT/arm-none-eabi/include/newlib-nano/newlib.h";
