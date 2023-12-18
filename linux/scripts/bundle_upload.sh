08c81b38c6d32ff7ea78c240b18b57e32ccfda17
DrunkBatya <drunkbatya.js@gmail.com>
2023-12-18 04:41:25 -0500

    Enable bundle-upload

--- linux/scripts/bundle_upload.sh
+++ linux/scripts/bundle_upload.sh
@@ -2,25 +2,40 @@
 
 set -euo pipefail;
 
-TOOLCHAIN_VERSION="24";
-GCC_VERSION="12.3";
+# external required variables:
+#   TOOLCHAIN_VERSION
+#   INDEXER_URl
+#   INDEXER_TOKEN
+echo "Bundling toolchain version: $TOOLCHAIN_VERSION";
 
-LINUX_ROOT="/toolchain";
-LINUX_OUTPUT_ROOT="linux-output-root";
-LINUX_OUTPUT_DIR="gcc-arm-none-eabi-$GCC_VERSION-x86_64-linux-flipper";
+LINUX_OUTPUT_ROOT=/toolchain/linux-output-root
+GCC_VERSION="12.3";
+ARCH="x86_64";
+
+LINUX_OUTPUT_ROOT_BASE_DIR="$(dirname "$LINUX_OUTPUT_ROOT")";
+LINUX_TOOLCHAIN_DIR_NAME="gcc-arm-none-eabi-$GCC_VERSION-$ARCH-linux-flipper";
+LINUX_OUTPUT_ROOT_NEW="$LINUX_OUTPUT_ROOT_BASE_DIR/$LINUX_TOOLCHAIN_DIR_NAME";
+LINUX_OUTPUT_FILE="$LINUX_OUTPUT_ROOT_NEW-$TOOLCHAIN_VERSION.tar.gz";
 
 function prepare_dir() {
-	mv "$LINUX_ROOT/$LINUX_OUTPUT_ROOT" "$LINUX_ROOT/$LINUX_OUTPUT_DIR";
-	printf "$TOOLCHAIN_VERSION" > "$LINUX_ROOT/$LINUX_OUTPUT_DIR/VERSION";
+	mv "$LINUX_OUTPUT_ROOT" "$LINUX_OUTPUT_ROOT_NEW";
+	printf "$TOOLCHAIN_VERSION" > "$LINUX_OUTPUT_ROOT_NEW/VERSION";
 }
 
 function make_bundle() {
-	pushd "$LINUX_ROOT";
+	pushd "$LINUX_OUTPUT_ROOT_BASE_DIR";
 	tar -czvf \
-		"$LINUX_ROOT/gcc-arm-none-eabi-$GCC_VERSION-x86_64-linux-flipper-$TOOLCHAIN_VERSION.tar.gz" \
-		"$LINUX_OUTPUT_DIR";
+		"$LINUX_OUTPUT_FILE" \
+		"$LINUX_TOOLCHAIN_DIR_NAME";
 	popd;
+}
+
+function upload_bundle() {
+    curl --fail -L -H "Token: $INDEXER_TOKEN" \
+        -F "files=@$LINUX_OUTPUT_FILE" \
+        "$INDEXER_URL"/toolchain/uploadfilesraw;
 }
 
 prepare_dir;
 make_bundle;
+upload_bundle;
---
+++ windows/scripts/bundle_upload.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail;
+
+# external required variables:
+#   TOOLCHAIN_VERSION
+#   INDEXER_URl
+#   INDEXER_TOKEN
+echo "Bundling toolchain version: $TOOLCHAIN_VERSION";
+
+WINDOWS_OUTPUT_ROOT=/toolchain/windows-output-root
+GCC_VERSION="12.3";
+ARCH="x86_64";
+
+WINDOWS_OUTPUT_ROOT_BASE_DIR="$(dirname "$WINDOWS_OUTPUT_ROOT")";
+WINDOWS_TOOLCHAIN_DIR_NAME="gcc-arm-none-eabi-$GCC_VERSION-$ARCH-windows-flipper";
+WINDOWS_OUTPUT_ROOT_NEW="$WINDOWS_OUTPUT_ROOT_BASE_DIR/$WINDOWS_TOOLCHAIN_DIR_NAME";
+WINDOWS_OUTPUT_FILE="$WINDOWS_OUTPUT_ROOT_NEW-$TOOLCHAIN_VERSION.tar.gz";
+
+function prepare_dir() {
+	mv "$WINDOWS_OUTPUT_ROOT" "$WINDOWS_OUTPUT_ROOT_NEW";
+	printf "$TOOLCHAIN_VERSION" > "$WINDOWS_OUTPUT_ROOT_NEW/VERSION";
+}
+
+function make_bundle() {
+	pushd "$WINDOWS_OUTPUT_ROOT_BASE_DIR";
+	tar -czvf \
+		"$WINDOWS_OUTPUT_FILE" \
+		"$WINDOWS_TOOLCHAIN_DIR_NAME";
+	popd;
+}
+
+function upload_bundle() {
+    curl --fail -L -H "Token: $INDEXER_TOKEN" \
+        -F "files=@$WINDOWS_OUTPUT_FILE" \
+        "$INDEXER_URL"/toolchain/uploadfilesraw;
+}
+
+prepare_dir;
+make_bundle;
+upload_bundle;
