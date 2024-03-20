47209e4c3331c7b23f465bc41ddae9234aa0d9d6
DrunkBatya <drunkbatya.js@gmail.com>
2024-03-20 11:32:47 -0400

    Migrate to flipper openocd fork, linux

--- linux/scripts/build-linux-flipper-tools.sh
+++ linux/scripts/build-linux-flipper-tools.sh
@@ -7,6 +7,9 @@ LINUX_OUTPUT_ROOT=/toolchain/linux-output-root
 LINUX_CONFIGURE_ROOT=/toolchain/linux-configure-root
 
 CPUS="$(grep -c processor /proc/cpuinfo )";
+ARCH="$(uname -m)";
+
+export PKG_CONFIG_PATH="$LINUX_OUTPUT_ROOT/lib/pkgconfig";
 
 function cleanup_relink() {
     local DIRECTORY;
@@ -14,6 +17,11 @@ function cleanup_relink() {
     find "$DIRECTORY" \( -name "*.a" -or -name "*.la" \) -delete;
     rm -rf "$DIRECTORY/share/man"
     relink.sh "$DIRECTORY";
+}
+
+function copy_libudev() {
+    mkdir -p "$LINUX_OUTPUT_ROOT/lib";
+    cp -r /usr/lib/x86_64-linux-gnu/libudev.so.1* "$LINUX_OUTPUT_ROOT/lib/";
 }
 
 function build_protobuf() {
@@ -50,5 +58,76 @@ function build_clang_format() {
     popd;
 }
 
+function build_libusb() {
+    rm -rf "$LINUX_CONFIGURE_ROOT/libusb";
+    mkdir -p "$LINUX_CONFIGURE_ROOT/libusb";
+    pushd "$LINUX_CONFIGURE_ROOT/libusb";
+    /toolchain/src/src/libusb/configure \
+        --prefix="$LINUX_OUTPUT_ROOT";
+    make "-j$CPUS";
+    make install;
+    popd;
+    cleanup_relink "$LINUX_OUTPUT_ROOT";
+}
+
+function build_hidapi() {
+    rm -rf "$LINUX_CONFIGURE_ROOT/hidapi";
+    mkdir -p "$LINUX_CONFIGURE_ROOT/hidapi";
+    pushd "$LINUX_CONFIGURE_ROOT/hidapi";
+    cmake \
+        -S "/toolchain/src/src/hidapi" \
+        "-DCMAKE_INSTALL_PREFIX=/toolchain/linux-output-root";
+    make "-j$CPUS";
+    make install;
+    popd;
+    cleanup_relink "$LINUX_OUTPUT_ROOT";
+}
+
+function build_openocd() {
+    rm -rf "$LINUX_CONFIGURE_ROOT/openocd";
+    mkdir -p "$LINUX_CONFIGURE_ROOT/openocd";
+    pushd "/toolchain/src/src/openocd";
+    ./bootstrap;
+    popd;
+    pushd "$LINUX_CONFIGURE_ROOT/openocd";
+    LDFLAGS="-L$LINUX_OUTPUT_ROOT/lib" CPPFLAGS="-I$LINUX_OUTPUT_ROOT/include"  LD_LIBRARY_PATH="$LINUX_OUTPUT_ROOT/lib" \
+        /toolchain/src/src/openocd/configure \
+            "--prefix=$LINUX_OUTPUT_ROOT" \
+            "--host=$ARCH-linux-gnu" \
+            "--target=$ARCH-linux-gnu" \
+            "--datarootdir=$LINUX_OUTPUT_ROOT" \
+            "--localedir=$LINUX_OUTPUT_ROOT/share/locale" \
+            "--disable-wextra" \
+            "--disable-werror" \
+            "--disable-gccwarnings" \
+            "--disable-doxygen-html" \
+            "--disable-doxygen-pdf" \
+            "--disable-debug" \
+            "--disable-dependency-tracking" \
+            "--enable-cmsis-dap" \
+            "--enable-dummy" \
+            "--enable-stlink" \
+            "--disable-zy1000-master" \
+            "--disable-zy1000" \
+            "--disable-ioutil" \
+            "--disable-minidriver-dummy" \
+            "--disable-parport-ppdev" \
+            "--enable-amtjtagaccel" \
+            "--enable-buspirate" \
+            "--enable-gw16012" \
+            "--enable-parport" \
+            "--enable-parport-giveio" \
+            "--enable-sysfsgpio";
+    LDFLAGS="-L$LINUX_OUTPUT_ROOT/lib" CPPFLAGS="-I$LINUX_OUTPUT_ROOT/include"  LD_LIBRARY_PATH="$LINUX_OUTPUT_ROOT/lib" \
+        make "-j$CPUS";
+    make install-strip;
+    popd;
+    cleanup_relink "$LINUX_OUTPUT_ROOT";
+}
+
 build_protobuf;
 build_clang_format;
+copy_libudev;
+build_libusb;
+build_hidapi;
+build_openocd;
